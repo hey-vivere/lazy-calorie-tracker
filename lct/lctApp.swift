217d8c84bf8eb7e3dfb804b02494c1ff8775288b
Nikola Klipa <klipa.nikola@gmail.com>
2025-10-13 12:33:42 +0700

    Initial Commit

---
+++ lct/lctApp.swift
@@ -0,0 +1,17 @@
+//
+//  lctApp.swift
+//  lct
+//
+//  Created by Nikola Klipa on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct lctApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
